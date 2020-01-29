97962ea5c04e8f40e18b49c53acf1f724035e0fc
Steve Krueger <stevekrueger52@gmail.com>
2020-01-29 18:42:07 -0500

    I committed the sin of working in dev. Don't do what I just did.

---
+++ Assets/Scripts/AnimCurveSet.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class AnimCurveSet
+{
+    public bool HeadBob;
+    public bool ItemBob;
+    
+    [Space(5)]
+    [Range(0, 5)] public float headScale;      // How prominent the curves should be
+    [Range(0, 5)] public float itemScale;
+    [Range(0, 5)] public float loopTime;   // How long the loop should take
+    [Range(-1,1)] public float itemDelay;  // How long the delay between head/held items should be
+        
+    [Space(5)]
+    public AnimationCurve x; 
+    public AnimationCurve y;
+    public AnimationCurve z;
+
+    private float head;
+
+    public Vector3 getHeadOffset(float increment)
+    {
+        head = head + increment % loopTime;
+        if (HeadBob)
+            return new Vector3(x.Evaluate(head), y.Evaluate(head), z.Evaluate(head)) * headScale;
+        return Vector3.zero;
+    }
+    
+    public Vector3 getItemOffset(float increment)
+    {
+        head = head + increment % loopTime;
+        float tempHead = head + itemDelay % loopTime;
+        if (ItemBob)
+            return new Vector3(x.Evaluate(tempHead), y.Evaluate(tempHead), z.Evaluate(tempHead)) * itemScale;
+        return Vector3.zero;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,71 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Range(0,20)]    public float maxSpeed;         // Maximum attainable velocity (omnidirectional)
+    [Range(0,20)]    public float maxDeltaSpeed;    // Maximum attainable acceleration (omnidirectional)
+    [Range(0,1)]     public float movementDeadzone;
+
+    public GameObject heldItems;
+    private Vector3 handAnchor;
+
+    // See Bottom for implementation
+    public AnimCurveSet HeadBob;
+
+    private Vector3 vel = Vector2.zero;
+    private Vector3 accel = Vector2.zero;
+    private Rigidbody rb;
+    private Camera cam;
+    private Vector3 camAnchor;
+    
+    
+// Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        cam = GetComponentInChildren<Camera>();
+        camAnchor = cam.transform.localPosition;
+        handAnchor = heldItems.transform.localPosition;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+        UpdateMove();
+        if (vel.magnitude != 0)
+        {
+            cam.transform.localPosition = camAnchor + HeadBob.getHeadOffset(Time.deltaTime);
+            heldItems.transform.localPosition = handAnchor + HeadBob.getItemOffset(0);
+        }
+    }
+
+    void UpdateMove()
+    {
+        // Get Input, scale to Acceleration
+        Vector3 steering = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        steering = steering.magnitude > 1 ? steering.normalized : steering;
         
+        // Adjust for local rotation
+        steering = steering.x * transform.right + steering.z * transform.forward;
+
+        if (steering.magnitude < movementDeadzone) // No input, stop
+            accel = vel.magnitude > maxDeltaSpeed ? -vel.normalized * maxDeltaSpeed : -vel;
+        else
+            accel = steering * maxDeltaSpeed;
+        
+        // Apply Acceleration
+        vel = Vector3.ClampMagnitude(vel + accel, maxSpeed);
+        
+        // Apply Velocity
+        rb.velocity = vel;
     }
 }
