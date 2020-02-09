e268c2cebb3eb41da145218a3eb05a3cd08ffba5
Steve Krueger <stevekrueger52@gmail.com>
2020-02-09 16:00:45 -0500

    Barebones API up

---
+++ Assets/Scripts/Junction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class Junction : MonoBehaviour
+{
+
+    public Collider approachTrigger;
+    public Collider enterTrigger;
+    public Stressor preAlarm;
+    public Stressor postAlarm;
+    public Exit[] exits;
+    
+    [Serializable]
+    public struct Exit
+    {
+        public float angle;
+        public Junction next;
+    }
+    
+}
---
+++ Assets/Scripts/Stressor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stressor : MonoBehaviour
+{
+    //TODO dummy class for base Stressor instantiation
+
+    public void OnSight()
+    {
+        // play animation when player enters adjacent junction
+    }
+
+    public void OnApproach()
+    {
+        // play animation when player approaches junction
+    }
+
+    public void OnEnter()
+    {
+        // play animation when player enters junction
+    }
+}
