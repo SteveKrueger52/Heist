20edb49611147650263ced5e9a16ac75cf353b12
Steve Krueger <stevekrueger52@gmail.com>
2020-02-09 15:37:26 -0500

    Spacebar now toggles map visibility

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,11 +8,15 @@ public class Map : MonoBehaviour
     public GameObject mesh;
     public Vector3 activeLocation;
     public Vector3 hiddenLocation;
+    public float transitionSpeed;
+
+    private bool hidden = true;
+    private float timer;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.localPosition = hiddenLocation;
     }
 
     // Update is called once per frame
@@ -19,7 +24,18 @@ public class Map : MonoBehaviour
     {
         //TODO Spacebar [ GetButtonDown("Submit") ] -> Toggle map visibility, seek in/out from bottom and
         //TODO adjust active-ness when fully offscreen.
+
+        if (Input.GetButtonDown("Submit"))
+            hidden = !hidden;
         
+        if (hidden && 0 < timer || !hidden & timer < transitionSpeed)
+            timer += Time.deltaTime * (hidden ? -1 : 1);
+        if (timer < 0)
+            timer = 0;
+        if (timer > transitionSpeed)
+            timer = transitionSpeed;
+
+        transform.localPosition = Vector3.Lerp(hiddenLocation, activeLocation, timer/transitionSpeed);
     }
 
     void GetHoldAngle()
